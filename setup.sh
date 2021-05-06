183e51bd5b6a8ce89a5ca8b8a19a8b701b6ef907
thiago-scherrer <eu@thiago.info>
2021-05-06 08:43:09 -0300

    :sparkles: add vim build

--- setup.sh
+++ setup.sh
@@ -5,43 +5,65 @@ function aptSetup() {
 	sudo apt update \
 	&& sudo apt full-upgrade -y \
 	&& sudo apt install \
+		build-essential \
+		cmake \
+		cura \
+		curl \
+		exuberant-ctags \
+		gimp \
 		git \
+		htop \
+		imagemagick \
+		irssi \
 		libavcodec-extra \
 		libboost-all-dev \
 		libboost-dev \
 		libboost-filesystem-dev \
+		libboost-filesystem-dev \
+		libboost-iostreams-dev \
+		libboost-locale-dev \
+		libboost-log-dev \
+		libboost-regex-dev \
+		libboost-thread-dev \
+		libcereal-dev \
+		libcurl4-openssl-dev \
+		libdbus-1-dev \
+		libeigen3-dev \
 		libfreetype-dev \
+		libgmpxx4ldbl \
+		libgtk-3-dev \
+		libgtk2.0-dev \
+		libncurses5-dev \
+		libncursesw5-dev \
+		libnlopt-cxx-dev \
+		libnlopt-dev \
+		libopenvdb-dev \
 		libsdl2-dev \
+		libtbb-dev \
+		libudev-dev \
+		libwxgtk3.0-dev \
+		libwxgtk3.0-gtk3-dev \
 		libz-dev libpng-dev \
 		lm-sensors \
 		nasm \
-		ubuntu-restricted-extras \
-		vlc* \
-		build-essential \
-		curl \
-		exuberant-ctags \
-		gimp \
-		htop \
-		universal-ctags \
-		zsh \
-		ranger \
-	    vim-gtk \
-		cura \
-	    terminator \
-		xfce4-screenshooter \
-		irssi \
-		zip \
-		unzip \
-		xclip \
-		x11-apps \
-		vim-gtk \
-		openvpn \
 		nodejs \
 		npm \
-		libncurses5-dev \
-		libncursesw5-dev \
-		xorg-dev \
+		openvpn \
+		pkg-config \
 		python3-dev \
+		ranger \
+		ubuntu-restricted-extras \
+		universal-ctags \
+		unzip \
+		vlc* \
+		x11-apps \
+		xclip \
+		xfce4-screenshooter \
+		xorg-dev \
+		zip \
+		zlib1g-dev \
+		zsh \
+	    terminator \
 
 	npm install --global yar
 }
@@ -59,6 +81,24 @@ function zshSetup () {
 }
 
 function vimSetup () {
+	git clone git@github.com:vim/vim.git vim_tmp
+	cd vim_tmp
+	sudo apt remove vim* -y
+	export VIMRUNTIMEDIR=/usr/share/vim/vim82
+	./configure --with-features=huge \
+		--enable-multibyte \
+        --enable-rubyinterp=yes \
+        --enable-python3interp=yes \
+        --with-python3-config-dir=$(python3-config --configdir) \
+        --enable-perlinterp=yes \
+        --enable-luainterp=yes \
+        --enable-gui=gtk2 \
+        --enable-cscope
+	make -j2
+	sudo make install
+
+	cd ..
+	rm -rf vim_tmp
 	cp vim/.vimrc ~/.vimrc
 }
 
