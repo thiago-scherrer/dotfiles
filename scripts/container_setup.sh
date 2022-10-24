f947e03b560bd20eee5c80768a8829f0ab952428
Thiago Scherrer <eu@thiago.info>
2022-10-24 06:40:49 -0300

    Add helm plugins

--- scripts/container_setup.sh
+++ scripts/container_setup.sh
@@ -139,6 +139,19 @@ function k8s () {
 	wget https://github.com/istio/istio/releases/download/${istio_version}/istioctl-${istio_version}-linux-amd64.tar.gz  \
 	&& tar -C /bin/ -xvf istioctl-${istio_version}-linux-amd64.tar.gz \
 	&& rm -f istioctl-${istio_version}-linux-amd64.tar.gz
+
+	(
+	  set -x; cd "$(mktemp -d)" &&
+	  OS="$(uname | tr '[:upper:]' '[:lower:]')" &&
+	  ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')" &&
+	  KREW="krew-${OS}_${ARCH}" &&
+	  curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/${KREW}.tar.gz" &&
+	  tar zxvf "${KREW}.tar.gz" &&
+	  ./"${KREW}" install krew
+	)
+
+	kubectl krew install tree
+
 }
 
 function awsInstall () {
